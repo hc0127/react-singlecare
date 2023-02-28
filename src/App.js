3e05d35d4adef76cd56b646701d3d8ea1e9b505c
Roman Shalak <shaksev@gmail.com>
2023-02-28 06:38:17 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Virtual from "./pages/virtual";
+import Medicine from "./pages/medicine";
+import { createBrowserHistory } from "history";
+
+function App() {
+  const history = createBrowserHistory();
+
+  return (
+    <Routes history={history}>
+      <Route path="/" element={<Virtual />} />
+      <Route path="/virtualme" element={<Virtual />} />
+      <Route path="/virtualme/:medicine" element={<Medicine />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/medicine.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'; 
+import {
+  MDBContainer,
+  MDBBtn
+} from "mdb-react-ui-kit";
+
+export default function Medicine(props) {
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <MDBContainer>
+      <MDBBtn>Click me</MDBBtn>
+    </MDBContainer>
+  );
+}
